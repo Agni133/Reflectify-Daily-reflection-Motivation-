d77c94fe2c816295c2f848b40ccee93325f57bfe
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-05-29 16:27:22 +0530

    added auth route

---
+++ Backend/src/routes/auth.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import * as authController from "../controller/authController";
+
+
+
+
+const router = express.Router();
+router.post("/signup", authController.signup);
+
+router.post("/signin", authController.login);
+
+
+
+export default router;
