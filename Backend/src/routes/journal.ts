4e8905b5e533478ce361afc3d93d6a58aae4d427
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-05-29 17:08:41 +0530

    added journal ts

---
+++ Backend/src/routes/journal.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import { createJournal, deleteJournal, getuserJournal } from "../controller/journalController";
+
+import { authenticateToken } from "../middleware/authMiddleware";
+
+const router = Router();
+
+ router.post('/create',authenticateToken, createJournal);
+ router.get('/',authenticateToken,getuserJournal);
+ router.delete('/:id',authenticateToken,deleteJournal)
+   
+
+export default router;
