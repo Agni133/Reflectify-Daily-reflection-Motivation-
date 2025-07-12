e0fdb6de09eb6fd4bbf93319c0e4db8adc5f32e1
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-07-13 03:23:23 +0530

    updated cta file

--- frontend/src/components/CTA.tsx
+++ frontend/src/components/CTA.tsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
       <h3 className="text-3xl font-bold mb-4">
          Ready to feel lighter & more focused?
       </h3>
-         <Link to="/Login">
+         <Link to="/login">
           <Button className="mt-4 bg-slate-800 hover:bg-teal-700 text-white px-8 py-4 text-lg rounded-full shadow-lg hover:scale-105 transition-transform duration-300">
             Join now <ArrowRight className="ml-2 w-5 h-5"/>
           </Button> 
