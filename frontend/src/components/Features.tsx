21e59ed9a216b2c5c5071c78c477dd42b2d54a15
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-07-17 15:04:44 +0530

    updated feature section

--- frontend/src/components/Features.tsx
+++ frontend/src/components/Features.tsx
@@ -10,24 +10,24 @@ export function Features() {
              transition={{duration:0.9}}
              className="max-w-5xl mx-auto text-center mb-12"
       >  
-     <h2 className="text-3xl font-semibold">Why Reflectify Me?</h2> <br />
+     <h2 className="text-black text-3xl font-semibold">Why Reflectify Me?</h2> <br />
       
       
       <div className="grid gap-8 md:grid-cols-3 max-w-5xl mx-auto">
         <div className="p-6 bg-white rounded-xl shadow hover:shadow-md transition">
-          <PenLine className="w-8 h-8 mx-auto mb-4 text-teal-600" />
+          <PenLine className="w-8 h-8 mx-auto mb-4 text-amber-900" />
           <h3 className="text-xl font-semibold mb-2">Daily Journaling</h3>   
           <p className="text-gray-600">Pour your heart out, keep it safe & private.</p>
         </div>
 
         <div className="p-6 bg-white rounded-xl shadow hover:shadow-md transition">
-          <Sparkles className="w-8 h-8 mx-auto mb-4 text-teal-600" />
+          <Sparkles className="w-8 h-8 mx-auto mb-4 text-amber-900" />
           <h3 className="text-xl font-semibold mb-2">Mood & Random Quotes Boost</h3>
           <p className="text-gray-600">Save  random quotes that spark joy, track how you truly feel.</p>
         </div>
 
         <div className="p-6 bg-white rounded-xl shadow hover:shadow-md transition">
-          <Heart className="w-8 h-8 mx-auto mb-4 text-teal-600" />
+          <Heart className="w-8 h-8 mx-auto mb-4 text-amber-900" />
           <h3 className="text-xl font-semibold mb-2">Mindful Habits</h3>
           <p className="text-gray-600">Build a simple daily ritual that lifts your mind.</p>
         </div>
