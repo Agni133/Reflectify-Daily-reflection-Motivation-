03f31fd70be29cbfe4b3fb9e1228370a31c0cf8a
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-07-23 03:22:02 +0530

    updated Cta section

--- frontend/src/components/CTA.tsx
+++ frontend/src/components/CTA.tsx
@@ -2,19 +2,18 @@ import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
-
- export function CTA(){
-   return (
-     <footer className="py-16 bg-blue-50 text-center px-6">
-      <h3 className="text-3xl font-bold mb-4">
-         Ready to feel lighter & more focused?
+export function CTA() {
+  return (
+    <footer className="py-20 text-center  from-indigo-800 via-purple-900 to-slate-900  text-white">
+      <h3 className="text-3xl md:text-4xl font-bold mb-6">
+        Ready to feel lighter & more focused?
       </h3>
-         <Link to="/login">
-          <Button className="mt-4 bg-slate-800 hover:bg-teal-700 text-white px-8 py-4 text-lg rounded-full shadow-lg hover:scale-105 transition-transform duration-300">
-            Join now <ArrowRight className="ml-2 w-5 h-5"/>
-          </Button> 
-         </Link>
-     </footer>
-   );
-  
+      <Link to="/register">
+        <Button className="mt-4 bg-blue-700 hover:bg-blue-800 text-white px-8 py-4 text-lg rounded-full shadow-xl hover:scale-105 transition-transform duration-300">
+          Join now <ArrowRight className="ml-2 w-5 h-5" />
+        </Button>
+      </Link>
+    </footer>
+  );
 }
+
