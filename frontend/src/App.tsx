ad1b372cb1a9019b6b1db75f8ce9a8fe5626005a
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-08-06 17:55:47 +0530

    updated App tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Landing from "@/pages/Landing";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 
 function App(){
@@ -15,6 +15,7 @@ return (
      <Route path ="/register" element={<Register/>}/>
      <Route path = "/login" element={<Login/>}/>
      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path ="/profile" element ={<Profile/>}/>
      </Routes>
     </Router>
 
