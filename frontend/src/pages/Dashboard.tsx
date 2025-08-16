a5b3da6c051d1d6a4d16d4184c9737d5714667d8
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-08-17 00:39:35 +0530

    updated dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import { useState,useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import axios from "@/lib/axios";
 import {motion} from "framer-motion"
-import {Profile} from "@/pages/Profile";
+ 
 
 
  interface Journals{
