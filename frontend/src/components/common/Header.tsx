93c95722bae2320a9d0eb25df5303c73e9e095ca
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-09-09 17:31:48 +0530

    added component dashboard header section

---
+++ frontend/src/components/common/Header.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Header(){
+
+return (  
+  <header className="w-full px-8 py-4 border-b border-slate-800 bg-slate-900/70 backdrop-blur-md">
+   <div className="flex items-center justify-between max-w-6xl mx-auto">
+    <div className="text-3xl italic text-center text-white font-semibold ">Reflectify Me</div>
+     <nav className="font-medium italic text-slate-300 space-x-8">
+     <Link to= "/" className="hover:text-blue-400">Home</Link>
+      <Link to= "/profile">Profile</Link> 
+     </nav>
+   </div>
+  </header>
+);
+
+}
