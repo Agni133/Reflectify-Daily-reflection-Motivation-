d96d9baf434d17cc066ce85889d9328c5ba648c8
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-11-20 23:26:54 +0530

    updated axios

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,11 +1,27 @@
 import axios from "axios";
 
+
 const instance = axios.create({
 
 baseURL : import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
  headers :{
     "Content-Type":"application/json",
  },
-})
+});
 
+instance.interceptors.request.use(
+
+(config)=>{
+
+ const token = localStorage.getItem("token")
+ if(token){
+ config.headers.Authorization =  `Bearer ${token}`
+ }
+ return config;
+}, 
+ 
+(error)=>{
+  return  Promise.reject(error);
+}
+)
 export default instance;  
