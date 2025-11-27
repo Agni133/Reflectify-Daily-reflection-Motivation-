e36c9368d26d7da4216f7c7e28ad945273f65304
Agni133 <105123072+Agni133@users.noreply.github.com>
2025-11-28 04:49:16 +0530

    updated profile ts

--- Backend/src/routes/profile.ts
+++ Backend/src/routes/profile.ts
@@ -1,8 +1,15 @@
 import express from  "express";
-import { updateProfilePic } from "../controller/profileController";
+import { getAvatar, updateAvatar, updateFont, updateProfilePic, updateTheme } from "../controller/profileController";
 import { upload } from "../middleware/upload";
+import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/upload", upload.single("profilePic") ,updateProfilePic)
+
+router.put("/profile/avatar",authenticateToken,updateAvatar);
+router.put("/proile/theme",authenticateToken,updateTheme);
+router.put("/profile/font",authenticateToken,updateFont);
+router.get("/avatar",getAvatar);
 export default router;
+
