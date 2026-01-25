5a6fc887260a6d7a0bf9689a7df1a75d431326de
Agni133 <105123072+Agni133@users.noreply.github.com>
2026-01-26 01:28:29 +0530

    added new url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,27 +1,22 @@
 import axios from "axios";
 
-
 const instance = axios.create({
-
-baseURL : import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
- headers :{
-    "Content-Type":"application/json",
- },
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-(config)=>{
-
- const token = localStorage.getItem("token")
- if(token){
- config.headers.Authorization =  `Bearer ${token}`
- }
- return config;
-}, 
- 
-(error)=>{
-  return  Promise.reject(error);
-}
-)
-export default instance;  
+export default instance;
