88f6d02264f5c531f64d22b9412ea36e901fb603
Agni133 <105123072+Agni133@users.noreply.github.com>
2026-01-26 17:57:24 +0530

    updated index ts file

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
